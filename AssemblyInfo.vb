761c842c1071fdd917da75ab818e1e68c1a9ed23
Youssef Pro <72635460+YoussefPro986@users.noreply.github.com>
2022-10-27 10:19:15 -0700

    project files
    Source code and project files

---
+++ AssemblyInfo.vb
@@ -0,0 +1,37 @@
+Imports System.Resources
+
+Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("Encrypt / Decrypt File (Using Rijndael)")> 
+<Assembly: AssemblyDescription("In cryptography, encryption is the process of transforming information (referred to as plaintext) using an algorithm (called cipher)")> 
+<Assembly: AssemblyCompany("Youssef Pro")> 
+<Assembly: AssemblyProduct("Encrypt / Decrypt File Tool")> 
+<Assembly: AssemblyCopyright("Copyright © Youssef Pro 2005")> 
+<Assembly: AssemblyTrademark("YP")> 
+<Assembly: CLSCompliant(True)> 
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("1DCAAD60-155C-420C-90C5-03005ABABD00")> 
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+
+<Assembly: AssemblyVersion("1.6.3.7")> 
+
+<Assembly: AssemblyFileVersionAttribute("4.8.4.3")> 
+<Assembly: NeutralResourcesLanguageAttribute("en-US")> 
